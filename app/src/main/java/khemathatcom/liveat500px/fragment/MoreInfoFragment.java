2f6daece9eae74adc1239b9b1edc909eda840469
Unfrez141 <Khemathat@khemmathats-MacBook-Pro.local>
2017-08-20 01:52:39 +0700

    addPager

--- app/src/main/java/khemathatcom/liveat500px/fragment/MoreInfoFragment.java
+++ app/src/main/java/khemathatcom/liveat500px/fragment/MoreInfoFragment.java
@@ -3,6 +3,8 @@ package khemathatcom.liveat500px.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,8 @@ import khemathatcom.liveat500px.R;
  */
 public class MoreInfoFragment extends Fragment {
 
+
+    ViewPager viewPager;
 
     public MoreInfoFragment() {
         super();
@@ -55,6 +59,28 @@ public class MoreInfoFragment extends Fragment {
         // Init 'View' instance(s) with rootView.findViewById here
         // Note: State of variable initialized here could not be saved
         //       in onSavedInstanceState
+
+        viewPager = (ViewPager) rootView.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new FragmentStatePagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position){
+                    case 0:
+                        return PhotoSummaryFragment.newInstance();
+                    case 1:
+                        return PhotoInfoFragment.newInstance();
+                    case 2:
+                        return PhotoTagsFragment.newInstance();
+                    default:
+                        return null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
 
 
     }
